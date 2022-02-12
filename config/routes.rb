c65e8a1e50a989989ad1bbd7a2115c4a4b1bd0e0
MJ Aguirre <michaelaquila1998@gmail.com>
2022-02-12 14:59:31 +0800

    Update todo_item.html.erb
    1. Update todo_item.html.erb
    2. Update todo_item.rb
    3. Update todo_items_controller.rb
    4. Update routes.rb
    5. Add migration to todo items

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   resources :journals
 
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+       patch :complete
+      end
+    end
   end
 
-  root "todo_lists#index"
-
+  root "journals#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_043630) do
+ActiveRecord::Schema.define(version: 2022_02_12_063136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_02_12_043630) do
     t.bigint "todo_list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
